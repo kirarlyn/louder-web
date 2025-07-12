e705e0016ac388e82fc49d79fe575de4548c22ac
kirarlyn <nguyenduykhoa363@gmail.com>
2025-07-12 10:00:38 +0700

    update

--- database.js
+++ database.js
@@ -60,35 +60,5 @@ const database = {
                 publishDate: '2025-07-12T01:30:00Z' // Vài giờ trước (8:30 sáng)
             },
         ]
-    },
-
-    'one-piece': {
-        title: 'One Piece',
-        author: 'Eiichiro Oda',
-        description: 'Vua Hải Tặc (ワンピース Wan Pīsu?), một bộ manga của tác giả Oda Eiichiro dành cho lứa tuổi thiếu niên. Câu chuyện về một cậu bé tên Monkey D. Luffy, được khuyến khích bởi thần tượng thời thơ ấu và hải tặc hùng mạnh "Tóc đỏ" Shanks, bắt đầu một cuộc hành trình từ Biển Đông để tìm kho báu huyền thoại, One Piece, và tự xưng là Vua Hải Tặc.',
-        cover: 'data/doraemon/cover.png', // Đường dẫn tới ảnh bìa
-        status: 'Đang tiến hành',
-        tags: ['Hành động', 'Phiêu lưu', 'Hài hước'],
-        chapters: [
-            {
-                name: 'Chapter 1: Romance Dawn',
-                pages: [
-                    'data/one-piece/chapter-1/01.jpg',
-                    'data/one-piece/chapter-1/02.jpg',
-                    'data/one-piece/chapter-1/03.jpg'
-                ],
-                publishDate: '2025-07-01T12:00:00Z' // ~1 tuần trước
-            },
-            {
-                name: 'Chapter 2: Romance Dawn',
-                pages: [
-                    'data/one-piece/chapter-1/01.jpg',
-                    'data/one-piece/chapter-1/02.jpg',
-                    'data/one-piece/chapter-1/03.jpg'
-                ],
-                publishDate: '2025-07-10T20:00:00Z' // ~2 ngày trước
-            }
-            // thêm các chapter khác
-        ]
-    },
+    }
 };
